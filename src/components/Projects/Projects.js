6d1afb4a4447e6bd77e6b4d2987dfc25b2215a39
Christian Alexander <christian.alexanderato@gmail.com>
2024-06-23 19:58:51 -0700

    projects in progress

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,10 +1,38 @@
-import React from 'react'
+import "./Projects.css";
 
- function Projects() {
+function Projects() {
   return (
-    <div>
-      <h1>Projects</h1>
+    <div className="projects-table">
+      <h1>Work I've done:</h1>
+      <div className="container">
+        <div className="proj-img">
+          <img src="..\images\new-gunpla.png" alt="capstone" />
+        </div>
+        <div className="project-text">
+          <h1>E-Commerce Website</h1>
+          <p>
+            This project is a demonstration of a functioning e-commerce website
+            designed to sell and distribute Gundam model kits for a hobbyist.
+            Through various methods used, a user can simulate adding items to a
+            cart, proceeding with purchase with price and taxes in mind, which
+            leads to a order confirmation with a confirmation number. This
+            project is continuously updated.
+          </p>
+          <ul>
+            <li>React</li>
+            <li>TypeScript</li>
+            <li>MongoDB</li>
+            <li>Express.js</li>
+            <li>Material UI</li>
+          </ul>
+          <ul>
+            <li><a href="https://gunpla-galaxy.netlify.app/">Visit Page</a></li>
+            <li><a href="https://github.com/Adrick1210/Gunpla-front">Frontend Code</a></li>
+            <li><a href="https://github.com/Adrick1210/Gunpla-backend">Backend Code</a></li>
+          </ul>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
-export default Projects
+export default Projects;
